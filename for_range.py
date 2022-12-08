f0e9934e56df14b94583db5061823bcb38edb856
rotivo <vitorfernando@usp.br>
2022-12-08 07:51:34 -0300

    for_range.py

---
+++ for_range.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+for n in list(range(0, 100)):
+    print(n)
+print("Finalizado.")
+
+print('')
+
+print(list(range(0, 100)))
+
+print('')
+
+for n in list(range(1, 101)):
+    print(n)
+print('Finalizado.')
+
+print('')
+
+lista = [print(n) for n in list(range(1, 101))]
+print('Finalizado.')
