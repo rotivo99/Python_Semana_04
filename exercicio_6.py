95676b725853b3346af16c69a5107617089f8312
rotivo <vitorfernando@usp.br>
2022-12-08 07:51:14 -0300

    exercicio_6.py

---
+++ exercicio_6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Tentando ver quais valores são pares nessa lista.
+list_aula = [101,2,15,22,95,33,2,27,72,15,52]
+
+for n in list_aula:
+    if n % 2 == 0:
+        print(n)
+print("Finalizado.")
