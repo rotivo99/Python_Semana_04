4922c21d81d5250adf076f05a208d9ead95b3edc
rotivo <vitorfernando@usp.br>
2022-12-08 07:50:06 -0300

    exercicio_08_e_09.py

---
+++ exercicio_08_e_09.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+lista = list(range(0, 100))
+
+for x in lista:
+    print(x)
+print("Finalizado.")
+
+print('')
+
+lista_2 = list(range(0, 101))
+intervalo = [print(n) for n in lista_2]
+print('Finalizado.')
